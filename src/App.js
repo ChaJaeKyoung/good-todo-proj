32b3d7a8b45b4aefee4324d3721e6b7a9d1b17e7
jenny.cha <chajg91@naver.com>
2023-02-06 18:18:44 +0900

    23-02-06
    Calendar 이해해보기
    참조:https://sennieworld.tistory.com/74
    타입스크립트를 js에서 사용하려면 타입을 지우고 사용하면 된다.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import  reset  from "styled-reset";
+import { createGlobalStyle } from "styled-components";
+import { useMediaQuery } from "react-responsive";
+import { v4 as uuidv4 } from "uuid";
+import { format } from "date-fns";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+// 참고) 반응형 라이브러리 사용
+// 조건부 렌더링
+// {isMobile ? (
+//  모바일일 때 
+// ) : (
+//  PC일 때
+// )}
 
-export default App;
+// styled-components 를 사용하면 좋은점!
+// props로 넘겨줄 수 있다.
+// isMobile값으로 css도 다르게 처리 가능
+
+
+// global 스타일 지정
+
+
+// function App() {
+//   return (
+  
+//   );
+// }
+
+// export default App;
